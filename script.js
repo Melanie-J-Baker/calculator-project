08f1d1aa7ba587a9685e7d8d3d27b15da64f50cc
Melanie-J-Baker <baker_mel@hotmail.com>
2022-09-24 14:16:44 +0100

    Create function to render display when calculation is entered. roughly outline how clear and delete functions will work but keep commented out for now - test after

--- script.js
+++ script.js
@@ -19,27 +19,139 @@ function operate() {
     switch (operator) {
         case '+':
             num1 += num2;
-            //convertNumber();
+            convertNumber();
             break;
         case '-':
             num1 -= num2;
-            //convertNumber();
+            convertNumber();
             break; 
         case 'x':
             num1 *= num2;
-            //convertNumber();
+            convertNumber();
             break;
         case '/':
             if(num1 <= 0) {
                 num1 = "Error!!!";
-                //renderDisplay();
+                renderDisplay();
                 break;
             }
             num1 /= num2;
-            //convertNumber();
+            convertNumber();
             break;
     }
 }
 
-// convertNumber() {};
-// renderDisplay() {};
+function convertNumber() {
+    num1 = roundNumber(num1);
+    num1 = num1.toString();
+    renderDisplay();
+};
+
+function numHandler(num) {
+    if (num1 !== "" && num2 !== "" && operator === "") {
+        num1 = "";
+        currentDisplay.textContent = num2;
+   }
+    if (num2.length <= 9) {
+        num2 += num;
+        currentDisplay.textContent = num2;
+    }
+};
+
+function opHandler(op) {
+    if (num1 === "") {
+        num1 = num2;
+        checkOperator(op);
+    } else if (num2 === "") {
+        checkOperator(op);
+    } else {
+        operate();
+        operator = op;
+        currentDisplay.textContent = "";
+        prevDisplay.textContent = num1 + " " + operator;
+    }
+};
+
+function checkOperator(op) {
+    operator = op;
+    prevDisplay.textContent = num1 + " " + operator;
+    currentDisplay.textContent = "";
+    num2 = "";
+};
+
+function roundNumber(num) {
+    return Math.round(num * 100000) / 100000;
+};
+
+function renderDisplay() {
+    if (num1.length <= 9) {
+        currentDisplay.textContent = num1;
+    } else {
+        currentDisplay.textContent = num1.slice(0, 9) + "...";
+    }
+    prevDisplay.textContent = "";
+    operator = "";
+    num2 = "";
+};
+
+/*function clearDisplay() {
+    num2 = "";
+    num1 = "";
+    operator = "";
+    currentDisplay.textContent = "0";
+    prevDisplay.textContent = "";
+}*/
+
+/*function delete() {
+    currentDisplay.textContent = "0";
+    num2 = "";
+};
+
+function handleDelete() {
+    if (num2 !== "") {
+        num2 = num2.slice(0, -1);
+        currentDisplay.textContent = num2;
+        if (num2 === "") {
+            currentDisplay.textContent = "0";
+        }
+    }
+    if (num2 === "" && num1 !== "" && operator === "") {
+        num1 = num1.slice(0, -1);
+        currentDisplay.textContent = num1;
+    }
+}*/
+
+numbers.forEach(btn => {
+    btn.addEventListener("click", e => {
+        numHandler(e.target.textContent);
+    });
+});
+
+operators.forEach(btn => {
+    btn.addEventListener("click", e => {
+        opHandler(e.target.textContent);
+    });
+});
+
+equals.addEventListener("click", () => {
+    if (num2 != "" && num1 != "") {
+        operate();
+    }
+});
+
+/*clear.addEventListener("click", clearDisplay)*/
+
+//function handleDecimal() {
+//    if (!num2.includes(".")) {
+//        num2 += ".";
+//        currentDisplay.textContent = num2;
+//    }
+//}
+
+//decimal.addEventListener("click", () => {
+//    handleDecimal();
+//})
+
+/*deleteLast.addEventListener("click", e => {
+    delete();
+})*/
